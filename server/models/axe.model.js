ea8626116747ba310a0fd52b7febc55890f91bf4
Dirk Kinley <dkinley@gmail.com>
2021-08-03 16:07:38 -0700

    update to model

--- server/models/axe.model.js
+++ server/models/axe.model.js
@@ -11,25 +11,27 @@ const AxeSchema = new mongoose.Schema({
         enum: [ "Fender", "Gibson", "Martin", "Taylor", "Ibanez", "Yamaha", "Epiphone", "Schecter", "Squier", "Other" ],
         // if it doesn't match one of these enums exactly it won't pass validaiton
         minlength: [0, "You need to select the make of the instrument"],
-        maxlength: [1, "You can only select up to three genres"],
+        maxlength: [1, "You can only select one make"],
     },
     model: {
         type: String,
-        enum: [ "Fender", "Gibson", "Martin", "Taylor", "Ibanez", "Yamaha", "Epiphone", "Schecter", "Squier", "Other" ],
+        enum: [ "Stratocaster", "Telecaster", "Duo-Sonic", "Les Paul", "SG", "Hummingbird", "Songwriter", "Other Electric", "Other Acoustic" ],
         // if it doesn't match one of these enums exactly it won't pass validaiton
-        minlength: [0, "You need to select the make of the instrument"],
-        maxlength: [1, "You can only select up to three genres"],
+        minlength: [0, "You need to select the model of the instrument"],
+        maxlength: [1, "You can only select one model"],
+    },
 
     year: {
         type: Number,
-        required: [ false, ""],
+        required: [ false, "1968"],
         min: [ 1930, "The instrument must be manufactured no earlier than 1930"],
     },
+    
     description: {
         type: String, 
         required: [ true, "You must input a description"],  //true - this is a required field, the 2nd part is the requirement
-        minlength: [10, "Tell us about any modifications, what string gauge it plays best with and anything else noteable"],
+        minlength: [10, "Mention modifications, what string gauge play best and anything else noteable"],
     },
 },
     { timestamps: true }); // need it! this is the options of the Schema, required
-    module.exports = mongoose.model("Lit", LitSchema);
+    module.exports = mongoose.model("Axe", AxeSchema);
